7fa7d06c0daefc84861f164afa4bbf869bccf844
johnta0 <22534726+johnta0@users.noreply.github.com>
2022-11-06 19:04:32 +0900

    Use prisma for ORM
    Close #2
    
    Add prisma packages
    Add prisma vscode extension
    Add sample schema
    Initial migration
    Implement resolver, service

---
+++ prisma/migrations/20221106091813_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Cat" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Cat_pkey" PRIMARY KEY ("id")
+);
--- src/cats/models/cat.model.ts
+++ src/cats/models/cat.model.ts
@@ -1,10 +1,10 @@
-import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType({ description: 'cat' })
 export class Cat {
   @Field((_type) => ID)
   id: string;
 
-  @Directive('@upper')
+  @Field((_type) => String)
   name: string;
 }
