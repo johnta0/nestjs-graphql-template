bdab27019623793beeb98ed47824ca6b70674291
johnta0 <22534726+johnta0@users.noreply.github.com>
2022-11-11 16:54:12 +0900

    Upgrade packages

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = Number(process.env.PORT) | 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   console.log(`Application is running on ${await app.getUrl()}`);
 }
 bootstrap();
